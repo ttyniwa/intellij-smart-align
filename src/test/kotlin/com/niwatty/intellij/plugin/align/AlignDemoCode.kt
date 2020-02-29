78ad5eba2f532b1367939e3baf1ea0304068aff2
niwatty <noencrypt@gmail.com>
2020-02-29 15:55:01 +0900

    add AlignDemoCode

---
+++ src/test/kotlin/com/niwatty/intellij/plugin/align/AlignDemoCode.kt
@@ -0,0 +1,21 @@
+package com.niwatty.intellij.plugin.align
+
+class AlignDemoCode {
+    fun hoge() {
+        var aaa = 1
+        var b = 2
+        b += 2
+
+        listOf(
+                listOf("hello", "world"),
+                listOf("say hello to", "world"),
+                listOf("good bye,", "word")
+        )
+
+        listOf(
+                "hello", "world", // comment
+                "hello world", "yes",   // comment
+                "good bye"
+        )
+    }
+}
